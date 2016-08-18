2068d52af6fbc05707527d49349ec3212f41f89a
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-08-18 17:52:26 +0300

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@
 * @Date:   2016-08-18T14:41:20+03:00
 * @Email:  pkiragu@cytonn.com
 * @Last modified by:   PKiragu
-* @Last modified time: 2016-08-18T17:09:37+03:00
+* @Last modified time: 2016-08-18T17:51:47+03:00
 */
 
 
 
 var gulp = require('gulp');
 var sass = require ('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log('Hello Phillo');
@@ -20,4 +21,8 @@ gulp.task('sass', function(){
   return gulp.src('css/styles.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('css/styles.scss', ['sass']);
 });
